c3d8e5a21c04046f708ce579e8a4adc1aed9a44f
Md. Gofran Khan <gofran0123@gmail.com>
2024-12-30 23:51:41 +0600

    logout added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -57,4 +57,23 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
+
+       /**
+     * Delete the user's account.
+     */
+    public function adminLogout(Request $request): RedirectResponse
+    {
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//Admin All route
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/logout', [ProfileController::class, 'adminLogout'])->name('admin.logout');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
