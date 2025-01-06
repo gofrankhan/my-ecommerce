67ff9cd4dce13ea4e2e9e3752216dda70b489fae
Md. Gofran Khan <gofran0123@gmail.com>
2025-01-07 01:12:51 +0600

    admin profile edit page part 1

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -19,37 +19,34 @@
 					  <div class="row">
 						<div class="col-6">						
 							<div class="form-group">
-								<h5>Basic Text Input <span class="text-danger">*</span></h5>
+								<h5>Admin Full Name <span class="text-danger">*</span></h5>
 								<div class="controls">
-									<input type="text" name="text" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
-								<div class="form-control-feedback"><small>Add <code>required</code> attribute to field for required validation.</small></div>
+									<input type="text" name="username" class="form-control" required="" value="{{ $userData->name }}" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
+								<div class="form-control-feedback"><small></small></div>
 							</div>
 							<div class="form-group">
-								<h5>Email Field <span class="text-danger">*</span></h5>
+								<h5>Admin Email <span class="text-danger">*</span></h5>
 								<div class="controls">
-									<input type="email" name="email" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
+									<input type="email" name="email" class="form-control" required="" value="{{ $userData->email }}" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
 							</div>
 							<div class="form-group">
-								<h5>Password Input Field <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="password" name="password" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
-							</div>
-							<div class="form-group">
-								<h5>Repeat Password Input Field <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="password" name="password2" data-validation-match-match="password" class="form-control" required=""> <div class="help-block"></div></div>
-							</div>
-							<div class="form-group">
-								<h5>File Input Field <span class="text-danger">*</span></h5>
-								<div class="controls">
-									<input type="file" name="file" class="form-control" required=""> <div class="help-block"></div></div>
+								<div class="text-xs-right">
+									<button type="submit" class="btn btn-rounded btn-info">Update</button>
+								</div>
 							</div>
 						</div>
-                        <div class=row>
-                            <div class="text-xs-right">
-                                <button type="submit" class="btn btn-rounded btn-info">Update</button>
-                            </div>
-                        </div>
+						<div class="col-6">	
+							<div class="form-group">
+								<h5>Admin Profile Image <span class="text-danger">*</span></h5>
+								<div class="controls">
+									<input type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
+							</div>
+							
+							<div class="col-md-6">
+								<img class="rounded-circle" src="{{ (!empty($userData->profile_photo_path))? url('upload/admin_images/'.$userData->profile_photo_path): asset('backend/images/user3-128x128.jpg')}}" alt="User Avatar">
+							</div>
+						</div>
+                        
 					</form>
 
 				</div>
