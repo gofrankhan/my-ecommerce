75f859731641a879e4138dad5158bd614e8fa722
Md. Gofran Khan <gofran0123@gmail.com>
2025-01-28 15:35:36 +0600

    completed the product details page

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -15,43 +15,48 @@ use Illuminate\Support\Facades\Hash;
 
 class IndexController extends Controller
 {
-    //
-    public function index(){
-		$categories = Category::orderBy('category_name_en','ASC')->limit(6)->get();
-		$products = Product::where('status',1)->orderBy('id','DESC')->limit(6)->get();
-		$sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
-        return view('frontend.index', compact('categories', 'sliders', 'products'));
-    }
+	//
+	public function index()
+	{
+		$categories = Category::orderBy('category_name_en', 'ASC')->limit(6)->get();
+		$products = Product::where('status', 1)->orderBy('id', 'DESC')->limit(6)->get();
+		$sliders = Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
+		return view('frontend.index', compact('categories', 'sliders', 'products'));
+	}
 
-    public function UserProfile(){
-        $id = Auth::user()->id;
-    	$user = User::find($id);
-    	return view('frontend.profile.user_profile',compact('user'));
-    }
+	public function UserProfile()
+	{
+		$id = Auth::user()->id;
+		$user = User::find($id);
+		return view('frontend.profile.user_profile', compact('user'));
+	}
 
-    public function UserLogout(){
-        Auth::logout();
-        return Redirect()->route('login');
-    }
+	public function UserLogout()
+	{
+		Auth::logout();
+		return Redirect()->route('login');
+	}
 
-    public function UserDashboard(){
-        $id = Auth::user()->id;
-    	$user = User::find($id);
-    	return view('frontend.profile.user_dashboard',compact('user'));
-    }
+	public function UserDashboard()
+	{
+		$id = Auth::user()->id;
+		$user = User::find($id);
+		return view('frontend.profile.user_dashboard', compact('user'));
+	}
 
-    public function UserProfileStore(Request $request){
-        $data = User::find(Auth::user()->id);
+	public function UserProfileStore(Request $request)
+	{
+		$data = User::find(Auth::user()->id);
 		$data->name = $request->name;
 		$data->email = $request->email;
 		$data->phone = $request->phone;
- 
+
 
 		if ($request->file('profile_photo_path')) {
 			$file = $request->file('profile_photo_path');
-			@unlink(public_path('upload/admin_images/'.$data->profile_photo_path));
-			$filename = date('YmdHi').$file->getClientOriginalName();
-			$file->move(public_path('upload/admin_images'),$filename);
+			@unlink(public_path('upload/admin_images/' . $data->profile_photo_path));
+			$filename = date('YmdHi') . $file->getClientOriginalName();
+			$file->move(public_path('upload/admin_images'), $filename);
 			$data['profile_photo_path'] = $filename;
 		}
 		$data->save();
@@ -62,15 +67,17 @@ class IndexController extends Controller
 		);
 
 		return redirect()->route('user.dashboard')->with($notification);
-    }
+	}
 
-    public function UserChangePassword(){
-    	$id = Auth::user()->id;
-    	$user = User::find($id);
-    	return view('frontend.profile.change_password',compact('user'));
-    }
+	public function UserChangePassword()
+	{
+		$id = Auth::user()->id;
+		$user = User::find($id);
+		return view('frontend.profile.change_password', compact('user'));
+	}
 
-    public function UserPasswordUpdate(Request $request){
+	public function UserPasswordUpdate(Request $request)
+	{
 
 		$validateData = $request->validate([
 			'oldpassword' => 'required',
@@ -78,39 +85,37 @@ class IndexController extends Controller
 		]);
 
 		$hashedPassword = Auth::user()->password;
-		if (Hash::check($request->oldpassword,$hashedPassword)) {
+		if (Hash::check($request->oldpassword, $hashedPassword)) {
 			$user = User::find(Auth::id());
 			$user->password = Hash::make($request->password);
 			$user->save();
 			Auth::logout();
 			return redirect()->route('user.logout');
-		}else{
+		} else {
 			return redirect()->back();
 		}
+	} // end method
 
-
-	}// end method
-
-	public function ProductDetails($id,$slug){
+	public function ProductDetails($id, $slug)
+	{
 		$product = Product::findOrFail($id);
 
 		$color_en = $product->product_color_en;
 		$product_color_en = explode(',', $color_en);
 
-		$color_hin = $product->product_color_hin;
-		$product_color_hin = explode(',', $color_hin);
+		$color_bn = $product->product_color_bn;
+		$product_color_bn = explode(',', $color_bn);
 
 		$size_en = $product->product_size_en;
 		$product_size_en = explode(',', $size_en);
 
-		$size_hin = $product->product_size_hin;
-		$product_size_hin = explode(',', $size_hin);
+		$size_bn = $product->product_size_bn;
+		$product_size_bn = explode(',', $size_bn);
 
-		$multiImag = MultiImg::where('product_id',$id)->get();
+		$multiImag = MultiImg::where('product_id', $id)->get();
 
 		$cat_id = $product->category_id;
-		$relatedProduct = Product::where('category_id',$cat_id)->where('id','!=',$id)->orderBy('id','DESC')->get();
-	 	return view('frontend.product.product_details_test',compact('product','multiImag','product_color_en','product_color_hin','product_size_en','product_size_hin','relatedProduct'));
-
+		$relatedProduct = Product::where('category_id', $cat_id)->where('id', '!=', $id)->orderBy('id', 'DESC')->get();
+		return view('frontend.product.product_details', compact('product', 'multiImag', 'product_color_en', 'product_color_bn', 'product_size_en', 'product_size_bn', 'relatedProduct'));
 	}
 }
