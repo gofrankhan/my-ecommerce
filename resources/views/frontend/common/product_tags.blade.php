9be72a97e5f38d47a6efbe159d1eaa6be26a51d3
Md. Gofran Khan <gofran0123@gmail.com>
2025-01-30 13:15:13 +0600

    fixed multiple tag showing issue

--- resources/views/frontend/common/product_tags.blade.php
+++ resources/views/frontend/common/product_tags.blade.php
@@ -1,26 +1,35 @@
 @php
-    $tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
-    $tags_bn = App\Models\Product::groupBy('product_tags_bn')->select('product_tags_bn')->get();
+    $tags_en = App\Models\Product::pluck('product_tags_en')->toArray();
+
+    $allTags_en = [];
+    foreach ($tags_en as $tagString) {
+        $individualTags = explode(',', $tagString);
+        $allTags_en = array_merge($allTags_en, $individualTags);
+    }
+    $uniqueTags_en = array_values(array_unique($allTags_en));
+
+    $tags_bn = App\Models\Product::pluck('product_tags_bn')->toArray();
+    $allTags_bn = [];
+    foreach ($tags_bn as $tagString) {
+        $individualTags = explode(',', $tagString);
+        $allTags_bn = array_merge($allTags_bn, $individualTags);
+    }
+    $uniqueTags_bn = array_values(array_unique($allTags_bn));
+
 @endphp
 <div class="sidebar-widget product-tag wow fadeInUp">
     <h3 class="section-title">Product tags</h3>
     <div class="sidebar-widget-body outer-top-xs">
         <div class="tag-list">
             @if (session()->get('language') == 'bangla')
-                @foreach ($tags_bn as $tag)
-                    @php $taglist = explode(',', $tag->product_tags_bn)@endphp
-                    @foreach ($taglist as $tag1)
-                        <a class="item active" title="Phone" href="{{ url('product/tag/' . $tag1) }}">
-                            {{ $tag1 }}</a>
-                    @endforeach
+                @foreach ($uniqueTags_bn as $tag)
+                    <a class="item active" title="Phone" href="{{ url('product/tag/' . $tag) }}">
+                        {{ $tag }}</a>
                 @endforeach
             @else
-                @foreach ($tags_en as $tag)
-                    @php $taglist = explode(',', $tag->product_tags_en)@endphp
-                    @foreach ($taglist as $tag1)
-                        <a class="item active" title="Phone" href="{{ url('product/tag/' . $tag1) }}">
-                            {{ $tag1 }}</a>
-                    @endforeach
+                @foreach ($uniqueTags_en as $tag)
+                    <a class="item active" title="Phone" href="{{ url('product/tag/' . $tag) }}">
+                        {{ $tag }}</a>
                 @endforeach
             @endif
         </div>
