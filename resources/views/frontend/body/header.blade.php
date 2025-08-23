94dbc2451649ada6b069a55325432dc87744fe62
Md. Gofran Khan <gofran0123@gmail.com>
2025-08-23 16:04:18 +0600

    Site settings frontend part done

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -104,14 +104,17 @@
     </div>
     <!-- /.header-top -->
     <!-- ============================================== TOP MENU : END ============================================== -->
+    @php
+         $setting = App\Models\SiteSettings::find(1);
+          @endphp
     <div class="main-header">
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-2 logo-holder">
                     <!-- ============================================================= LOGO ============================================================= -->
-                    <div class="logo"> <a href="{{ url('/') }}"> <img
-                                src="{{ asset('frontend/assets/images/logo.png') }}" alt="logo"> </a> </div>
+                    {{-- <div class="logo"> <a href="{{ url('/') }}"> <img src="{{ $setting->logo }}" alt="logo"> </a> </div> --}}
                     <!-- /.logo -->
+                    <div class="logo"> <a href="{{ url('/') }}"> <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="logo"> </a> </div>
                     <!-- ============================================================= LOGO : END ============================================================= -->
                 </div>
                 <!-- /.logo-holder -->
